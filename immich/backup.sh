969b713c9e0777567b96df1c07b8064eacc580ef
nemethomas <thomas.nemeth@posteo.ch>
2026-03-27 15:42:34 +0100

    backup auf batchverarbeitung

--- immich/backup.sh
+++ immich/backup.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-cd /home/tom/docker
+rclone copy /home/tom/docker/immich/upload/upload/ dropbox:backup/immich/upload/ \
+  --dropbox-batch-mode async \
+  --dropbox-batch-size 100 \
+  --transfers 2 \
+  --tpslimit 8 \
+  --retries 10 \
+  --retries-sleep 30s
 
-rclone copy /home/tom/docker/immich/upload/upload/ dropbox:backup/immich/upload/
-rclone copy /home/tom/docker/immich/upload/thumbs/ dropbox:backup/immich/thumbs/
-docker compose exec -T immich-db pg_dump -U immich immich > /home/tom/docker/immich/db_backup.sql
+rclone copy /home/tom/docker/immich/upload/thumbs/ dropbox:backup/immich/thumbs/ \
+  --dropbox-batch-mode async \
+  --dropbox-batch-size 100 \
+  --transfers 2 \
+  --tpslimit 8 \
+  --retries 10 \
+  --retries-sleep 30s
+
+docker compose -f /home/tom/docker/docker-compose.yml exec -T immich-db pg_dump -U immich immich > /home/tom/docker/immich/db_backup.sql
+
 rclone copy /home/tom/docker/immich/db_backup.sql dropbox:backup/immich/db/
